03fba0571cdc5d8bb2b480240bf4538c00ff7424
dosantosdev <dosantosdev@gmail.com>
2026-09-04 16:13:43 -0300

    feat: update NewOpinion component to integrate OpinionsContext and submit opinions to backend

--- src/components/NewOpinion.jsx
+++ src/components/NewOpinion.jsx
@@ -1,7 +1,11 @@
-import { useActionState } from "react";
+import { useActionState, use } from "react";
+
+import { OpinionsContext } from "../store/opinions-context.jsx";
 
 export function NewOpinion() {
-  function shareOpinionAction(prevState, formData) {
+  const { addOpinion } = use(OpinionsContext);
+
+  async function shareOpinionAction(prevState, formData) {
     const title = formData.get("title");
     const body = formData.get("body");
     const userName = formData.get("userName");
@@ -32,7 +36,7 @@ export function NewOpinion() {
     }
 
     // submit to backend
-
+    await addOpinion({ title, body, userName });
     return { erros: null };
   }
 
